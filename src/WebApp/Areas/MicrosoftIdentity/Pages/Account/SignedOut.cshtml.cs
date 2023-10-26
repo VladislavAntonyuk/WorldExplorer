90eb4afeba1513d994936bd864da9cac7e5466d5
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2023-10-27 01:44:02 +0300

    Update packages

---
+++ src/WebApp/Areas/MicrosoftIdentity/Pages/Account/SignedOut.cshtml.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Areas.MicrosoftIdentity.Pages.Account;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+[AllowAnonymous]
+public class SignedOutModel : PageModel
+{
+	public IActionResult OnGet()
+	{
+		return LocalRedirect("~/");
+	}
+}
