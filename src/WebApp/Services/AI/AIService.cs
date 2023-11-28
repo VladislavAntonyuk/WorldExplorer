8d93d6227f3fe8e60d71ddf57b713db102ec96c5
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2023-11-29 00:11:48 +0200

    Remove unnecessary import and update API endpoint compatibility

--- src/WebApp/Services/AI/AIService.cs
+++ src/WebApp/Services/AI/AIService.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Options;
 using OpenAI_API;
 using OpenAI_API.Chat;
 using OpenAI_API.Models;
-using Place;
 using Shared.Models;
 
 public class AiService(IOptions<AiSettings> aiSettings, ILogger<AiService> logger) : IAiService
@@ -20,11 +19,8 @@ public class AiService(IOptions<AiSettings> aiSettings, ILogger<AiService> logge
 		var generalPrompt = $$"""
 		                      You are a tour guide with a great knowledge of history. Tell me about 5 places near the following location: Latitude: {{location.Latitude}},Longitude: {{location.Longitude}}.
 		                      As they are famous places you must know their coordinates. Provide location as accurately as possible.
-		                      Places must be located within {{DistanceConstants.NearbyDistance}} meters from the location. 
-		                      If there are no places in the area, increase the distance but not more than {{DistanceConstants.NearbyDistance * 10}} meters. 
-		                      If there are still no places, return an empty list. Location is more important than number of places, so it's better to return fewer places than inaccurate locations.
 		                      Format the output in JSON format with the next properties: name, description, location (longitude, latitude).
-		                      Provide as detailed information in the description as possible. The description must contain a lot of text (at least 1000 words).
+		                      Provide as detailed information in the description as possible. The description for each place must contain a lot of text (at least 1000 words).
 		                      The output must contain only JSON because I will parse it later. Do not include any information or formatting except valid JSON.
 		                      Example:
 		                      [
@@ -35,10 +31,10 @@ public class AiService(IOptions<AiSettings> aiSettings, ILogger<AiService> logge
 		                          }
 		                      ]
 		                      """;
-		// https://platform.openai.com/docs/models/model-endpoint-compatibility
 		var api = aiSettings.Value.Provider == "Azure"
 			? OpenAIAPI.ForAzure("", "", new APIAuthentication(aiSettings.Value.ApiKey))
 			: new OpenAIAPI(aiSettings.Value.ApiKey);
+		// https://platform.openai.com/docs/models/model-endpoint-compatibility
 		var result = await api.Chat.CreateChatCompletionAsync(new[]
 		{
 			new ChatMessage(ChatMessageRole.Assistant, generalPrompt)
