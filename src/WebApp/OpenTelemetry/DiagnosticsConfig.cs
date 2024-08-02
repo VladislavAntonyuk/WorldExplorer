8599d49cc8af1bd94a4bae16a7face7bce551a7b
Vladislav Antonyuk <vladislav.antonyuk@dataart.com>
2024-08-02 14:34:16 +0300

    Add .NET Aspire

---
+++ src/WebApp/OpenTelemetry/DiagnosticsConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace WorldExplorer.Api.OpenTelemetry;
+
+public static class DiagnosticsConfig
+{
+    public const string ServiceName = "WorldExplorer";
+}
---
+++ src/WorldExplorer.AppHost/Program.cs
@@ -0,0 +1,14 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var redis = builder.AddRedis("redis")
+                   .WithImageTag("latest");
+
+var database = builder.AddSqlServer("database")
+                      .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+                      .PublishAsAzureSqlDatabase();
+
+builder.AddProject<Projects.WebApp>("webapp")
+       .WithReference(redis)
+       .WithReference(database);
+
+builder.Build().Run();
