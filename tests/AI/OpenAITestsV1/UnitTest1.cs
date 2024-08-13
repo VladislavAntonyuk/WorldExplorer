b9c4fa25c5405ee7a95eca9ed5903f56e247a735
Vladislav Antonyuk <vladislav.antonyuk@dataart.com>
2024-08-13 23:01:21 +0300

    Fix tests

---
+++ tests/AI/OpenAITestsV1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿namespace OpenAITestsV1;
+
+using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+public class UnitTest1
+{
+	private readonly ITestOutputHelper testOutputHelper;
+
+	public UnitTest1(ITestOutputHelper testOutputHelper)
+	{
+		this.testOutputHelper = testOutputHelper;
+	}
+
+	[Fact]
+	public async Task Test1()
+	{
+		var client = new OpenAI_API.OpenAIAPI(new APIAuthentication("API-KEY"));
+		client.ApiUrlFormat = "https://free.gpt.ge/{0}/{1}";
+		client.ApiVersion = "v1";
+		client.Chat.DefaultChatRequestArgs.Model = Model.ChatGPTTurbo;
+		var result = await client.Chat.CreateChatCompletionAsync(new ChatMessage(ChatMessageRole.User, "hello"));
+		testOutputHelper.WriteLine(result.Choices[0].Message.TextContent);
+	}
+}
---
+++ tests/AI/OpenAITestsV2/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿namespace OpenAITestsV1;
+
+using System.ClientModel;
+using OpenAI;
+using OpenAI.Chat;
+using Xunit;
+using Xunit.Abstractions;
+
+public class UnitTest1
+{
+	private readonly ITestOutputHelper testOutputHelper;
+
+	public UnitTest1(ITestOutputHelper testOutputHelper)
+	{
+		this.testOutputHelper = testOutputHelper;
+	}
+
+	[Fact]
+	public async Task Test1()
+	{
+		var client = new OpenAIClient(new ApiKeyCredential("API-KEY"),
+		                              new OpenAIClientOptions()
+		                              {
+										  Endpoint = new Uri("https://free.gpt.ge/v1")
+									  });
+		var chatClient = client.GetChatClient("gpt-3.5-turbo");
+		var result = await chatClient.CompleteChatAsync(new UserChatMessage("hello"));
+		testOutputHelper.WriteLine(result.Value.Content[0].ToString());
+	}
+}
